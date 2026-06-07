47cba38d5ee70968d5bf9d3db2fc96cedcf73741
vibhorguptaofficial <vibhorgupta245@gmail.com>
2026-06-07 23:09:09 +0530

    Uploaded my Python daily logic files

---
+++ hello.py
@@ -0,0 +1,14 @@
+# Triple quotes use karne se poem lines mein aati hai
+# 1. Triple quotes (''' या """) ka jadoo: 
+# Isse tum jitni marzi lines mein likho, Python waisa ka waisa hi print karega.
+print('''Johny Johny, Yes Papa,
+Eating sugar? No Papa.
+Telling lies? No Papa.
+Open your mouth, Ha! Ha! Ha!''')
+
+# 2. Multi-line Comment:
+# Jab tum bina 'print' ke triple quotes likhte ho, toh Python usey "Comment" manta hai.
+# Ye tab kaam aata hai jab tumhe poori kahani likhni ho par tum nahi chahte ki wo run ho.
+''' i am a good boy ''' 
+
+# Ek line wala comment hamesha hash (#) se shuru hota hai.
