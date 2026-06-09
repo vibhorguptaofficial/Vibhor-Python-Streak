25a36b821604564ba40b53132d4bc7ac4e46e0ef
vibhorguptaofficial <vibhorgupta245@gmail.com>
2026-06-09 22:13:43 +0530

    Uploaded my Python daily logic files

---
+++ quiz01.py
@@ -0,0 +1,54 @@
+a = 50
+b = 5
+
+
+addition = a + b
+print("The addition of", a, "and", b, "is:", addition)
+
+subtraction = a - b
+print("The subtraction of", a, "and", b, "is:", subtraction)
+
+multiplication = a * b
+print("The multiplication of", a, "and", b, "is:", multiplication)
+ 
+
+division = a / b
+print("The division of", a, "and", b, "is:", division)
+
+a = 20
+b = 99
+# ye harry sir ne likha
+print("the value of", a , "+" ,99, "is: ", a + b )
+print("the value of", a , "-" ,99, "is: ", a - b )
+print("the value of", a , "*" ,99, "is: ", a * b )
+print("the value of", a , "/" ,99, "is: ", a / b )
+
+
+
+# 1. Variables ki value set kari: a mein 50 aur b mein 5 dala
+a = 50
+b = 5
+
+# Hisab-kitab (Calculations) aur unhe variables mein save karna
+addition = a + b
+print("The addition of", a, "and", b, "is:", addition)
+
+subtraction = a - b
+print("The subtraction of", a, "and", b, "is:", subtraction)
+
+multiplication = a * b
+print("The multiplication of", a, "and", b, "is:", multiplication)
+
+# Division hamesha floating point (decimal) mein answer deta hai (10.0)
+division = a / b
+print("The division of", a, "and", b, "is:", division)
+
+# 2. Harry Sir wala logic:
+# Yahan humne variables ki value badal di (a=20 aur b=99)
+a = 20
+b = 99
+# Isme humne calculation seedha print statement ke andar hi kar di
+print("the value of", a , "+" ,99, "is: ", a + b )
+print("the value of", a , "-" ,99, "is: ", a - b )
+print("the value of", a , "*" ,99, "is: ", a * b )
+print("the value of", a , "/" ,99, "is: ", a / b )
