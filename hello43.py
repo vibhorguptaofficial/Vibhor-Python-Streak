a6200f9e8d9ceb765ffc6f1c0a24e7d7a7d71814
vibhorguptaofficial <vibhorgupta245@gmail.com>
2026-06-22 13:09:37 +0530

    Uploaded my Python daily logic files

---
+++ hello43.py
@@ -0,0 +1,30 @@
+class person:
+    name = "vibhu"
+    ocuupation = "software engnineer"
+    networth = "10lakh"
+    def info(self):
+        print(f"{self.name} is a {self.ocuupation} and networth is {self.networth}")
+
+a = person()
+a.name = "chiku"
+a.ocuupation = "accountant"
+a.networth = "20lakh"
+a.info()
+b = person()
+b.name = "nimit"
+b.ocuupation = "HR"
+b.networth = "30lakh"
+b.info()
+c = person()
+c.name = "nikitaka"
+c.ocuupation = "manager"
+c.networth = "40lakh"
+c.info()
+d = person()
+d.name = "rahul"
+d.ocuupation = "teacher"
+d.networth = "50lakh"
+d.info()
+e = person()
+e.info()
+# print(a.name, a.ocuupation)
